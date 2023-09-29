1f4d32672608fc4cc3f9a3af0dc27f0b97f141ff
angelikavilde <angelikavilde@gmail.com>
2023-09-29 11:32:44 +0100

    added split_punctuation and started find_links function

--- regextexttools.py
+++ regextexttools.py
@@ -1,11 +1,14 @@
+""""""
+
 import re
 
 
-EMAIL_REGEX = r"\w(?:[-\w+\.]*\w)?@\w[-\w\.]*\.[a-z]+"
+EMAIL_REGEX = r"\w(?:[-\w+\.]*\w)?@\w[-\w\.]*\.[a-z]+" # TODO [-\w\.] -> (?:\.[-\w])* ish to avoid ".."
 
+# TODO - do we care about .lower() in below?
 
 def find_emails(text: str, starting_positions: bool=False, ending_positions: bool=False) -> list:
-    """Returns email(s) within provided text or empty list if none are found"""
+    """Returns email(s) within provided text or empty list if none are found""" # TODO not finished text
     if not starting_positions and not ending_positions:
         return re.findall(EMAIL_REGEX, text)
     email_objects = re.finditer(EMAIL_REGEX, text)
@@ -26,5 +29,35 @@ def verify_email(text: str) -> bool:
     return False
 
 
+LINK_EMAIL = r"(?:https?:\/\/(?:w{3}.)?|w{3}.)\w[-\w]+(?:\.\w+)+\S*"
+
+
+def find_links(text: str, starting_positions: bool=False, ending_positions: bool=False) -> list:
+    """"""
+    if not starting_positions and not ending_positions:
+        return re.findall(LINK_EMAIL, text)
+
+
+def split_punctuation(text: str) -> list[str]:
+    """"""
+    split_text = re.split(r"[,\s;.]+", text)
+    return list(filter(None, split_text))
+
+
 if __name__ == "__main__":
-    pass
+    a = """https://www.example/com    
+https://www.example.com    
+http://subdomain.example.co.uk   
+https://regexr.com/   
+https://www.example.com:8080/page   
+http://www.example.com/path/with/slashes  
+https://example.com?query=parameter 
+https://sub.example.com/ 
+https://docs.discord.red/en/latest/guide_slash_and_interactions.html 
+https://www.example.com:8080/page#section 
+https://www.example.co.uk.
+https://www.example.co.uk   
+http://subdomain.example.com/path?query=value#fragment"""
+
+    print(find_links(a))
+    print(split_punctuation("iuy"))
