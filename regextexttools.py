9384ae61c47fcf79b9c1dc3f13e25fb305587809
angelikavilde <angelikavilde@gmail.com>
2023-09-29 13:34:14 +0100

    added verify_link function

--- regextexttools.py
+++ regextexttools.py
@@ -5,42 +5,46 @@ import re
 
 EMAIL_REGEX = r"\w(?:[-\w+\.]*\w)?@\w[-\w\.]*\.[a-z]+" # TODO [-\w\.] -> (?:\.[-\w])* ish to avoid ".."
 
-# TODO - do we care about .lower() in below?
 
 def find_emails(text: str, starting_positions: bool=False, ending_positions: bool=False) -> list:
     """Returns email(s) within provided text or empty list if none are found""" # TODO not finished text
     if not starting_positions and not ending_positions:
-        return re.findall(EMAIL_REGEX, text)
-    email_objects = re.finditer(EMAIL_REGEX, text)
+        return re.findall(EMAIL_REGEX, text.lower())
+    email_objects = filter(None, re.finditer(EMAIL_REGEX, text.lower()))
     if starting_positions and ending_positions:
-        return list((match.start(), match.end() - 1) for match in email_objects if match)
+        return list((match.start(), match.end() - 1) for match in email_objects)
     if starting_positions:
-        return list(match.start() for match in email_objects if match)
+        return list(match.start() for match in email_objects)
     if ending_positions:
-        return list(match.end() - 1 for match in email_objects if match)
+        return list(match.end() - 1 for match in email_objects)
 
 
 def verify_email(text: str) -> bool:
     """Returns True/False depending on if the
     full text is correctly formatted email"""
-    found_email = re.fullmatch(EMAIL_REGEX, text.strip())
-    if found_email:
-        return True
-    return False
+    return bool(re.fullmatch(EMAIL_REGEX, text.strip()))
 
 
-LINK_EMAIL = r"(?:https?:\/\/(?:w{3}.)?|w{3}.)\w[-\w]+(?:\.\w+)+\S*"
+LINK_REGEX = r"(?:https?:\/\/(?:w{3}.)?|w{3}.)\w[-\w]+(?:\.\w+)+\S*"
 
 
-def find_links(text: str, starting_positions: bool=False, ending_positions: bool=False) -> list:
+def find_links(text: str, starting_positions: bool=False, ending_positions: bool=False, query=False) -> list:
     """"""
+    # query=True returns links with /... or ?query=answer...
     if not starting_positions and not ending_positions:
-        return re.findall(LINK_EMAIL, text)
+        return re.findall(LINK_REGEX, text)
+    pass
+
+
+def verify_link(text: str) -> bool:
+    """Returns True/False depending on if the
+    full text is correctly formatted link"""
+    return bool(re.fullmatch(LINK_REGEX, text.strip()))
 
 
 def split_punctuation(text: str) -> list[str]:
-    """"""
-    split_text = re.split(r"[,\s;.]+", text)
+    """Returns text split by stop punctuations"""
+    split_text = re.split(r"[,\s;\.]+", text)
     return list(filter(None, split_text))
 
 
