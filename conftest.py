ebfd83905ebd3bd4265da07fb0096c98449781b5
angelikavilde <angelikavilde@gmail.com>
2023-11-15 20:17:44 +0000

    added a long story for testing

--- conftest.py
+++ conftest.py
@@ -13,3 +13,15 @@ def email_text():
 def incorrect_inputs():
     """Returns a list of non string data types"""
     return [[], dict(), None, set(), 1, True, (1,2), 1.1]
+
+
+@fixture
+def long_text_for_tests():
+    """Returns a long story string"""
+    return """In a serene, sunlit meadow, 
+    Sarah savored the fragrant roses blooming in rows. 
+    As she strolled, a gentle breeze rustled through 
+    the colorful petals. The tranquil spell was broken when,
+    suddenly, Sarah spotted a mischievous squirrel scampering
+    up a tall oak. Enchanted, she marveled at nature's whimsical
+    ballet, feeling truly spellbound."""
